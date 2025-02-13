e04534cd97dd5c4e01b72831e795a65d938a6da2
010kai <danielgarciagarrido96@gmail.com>
2025-02-13 11:48:44 +0100

    Add files via upload

---
+++ ejerudemy2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "El usuario que esta ahora mismo ejecutando el script es $USER"
+
+echo "El shell que se esta utilizando ahora mismo es $BASH"
+
+echo "El escritorio en el que te encuentras ahora mismo es $PWD"
